4a980cf7647cd7256fc339a3909e7d8b93477f25
kook1932 <gkswjddyd4@gmail.com>
2019-04-03 21:40:35 +0900

    간단한 재귀 함수 구현
    팩토리얼, 피보나치, 이진 탐색

---
+++ Chap02_Recursion/Chap02_Recursion/02-1.c
@@ -0,0 +1,21 @@
+//02-1. 함수의 재귀적 호출의 이해
+//팩토리얼, 피보나치는 대표적인 재귀 함수
+
+//Recursive Factorial
+/*
+#include<stdio.h>
+
+int Factorial(int n)
+{
+	if (n == 0)
+		return 1;
+	else
+		return n * Factorial(n - 1);
+}
+
+int main(void)
+{
+	printf("9! = %d \n", Factorial(9));
+	return 0;
+}
+*/
---
+++ Chap02_Recursion/Chap02_Recursion/02-2.c
@@ -0,0 +1,52 @@
+//Àç±ÍÀÇ È°¿ë
+
+/*
+Fibonacci Fucntion
+
+#include<stdio.h>
+
+int Fibo(int n)
+{
+	if (n == 0)
+		return 0;
+	else if (n == 1)
+		return 1;
+	else
+		return Fibo(n - 1) + Fibo(n - 2);
+}
+
+int main(void)
+{
+	for (int i = 0; i < 15; i++)
+		printf("%d ", Fibo(i));
+	return 0;
+}
+*/
+
+//Recursive Binary Search
+#include<stdio.h>
+
+int Recursive_Bsearch(int arr[], int first, int last, int target)
+{
+	if (first > last)
+		return -1;
+	int mid = (first + last) / 2;
+
+	if (arr[mid] == target)
+		return mid;
+	else if (target < arr[mid])
+		return Recursive_Bsearch(arr, first, mid - 1, target);
+	else
+		return Recursive_Bsearch(arr, mid + 1, last, target);
+}
+
+int main(void)
+{
+	int arr[] = { 1,3,5,7,9 };
+	int idx = Recursive_Bsearch(arr, 0, sizeof(arr) / sizeof(int), 9);
+	if (idx == -1)
+		printf("Å½»ö ½ÇÆÐ \n");
+	else
+		printf("Å½»ö ÀÎµ¦½º : %d \n", idx);
+	return 0;
+}
