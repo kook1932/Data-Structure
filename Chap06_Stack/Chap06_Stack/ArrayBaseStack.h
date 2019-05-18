297022e4a632ba60333e8e5904209af578558464
kook1932 <gkswjddyd4@gmail.com>
2019-05-18 22:42:52 +0900

    배열 기반 Stack 구현
    배열을 기반으로 Stack의 ADT 구현 및 활용

---
+++ Chap06_Stack/Chap06_Stack/ArrayBaseStack.h
@@ -0,0 +1,26 @@
+#ifndef __AB_STACK_H__
+#define __AB_STACK_H__
+
+#define TRUE	1
+#define FALSE	0
+#define STACK_LEN	100
+
+typedef int Data;
+
+typedef struct _arrayStack
+{
+	Data stackArr[STACK_LEN];
+	int topIndex;
+} ArrayStack;
+
+typedef ArrayStack Stack;
+
+void StackInit(Stack * pstack);		// 스택의 초기화
+int SIsEmpty(Stack * pstack);		// 스택이 비었는지 확인
+
+void SPush(Stack * pstack, Data data);		// 스택의 push 연산
+Data SPop(Stack * pstack);		// 스택의 pop 연산
+Data SPeek(Stack * pstack);		// 스택의 최근의 값 반환, no 삭제
+
+#endif // !__AB_STACK_H__
+
