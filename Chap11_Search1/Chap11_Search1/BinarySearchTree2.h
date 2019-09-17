cff8895e676674cf04a7aacdfdf2411ccfc36bf3
kook1932 <gkswjddyd4@gmail.com>
2019-09-17 19:06:24 +0900

    Binary Search Tree에 Delete 기능 추가
    Binary Tree에 노드 제거하는 기능, 노드 연결하는 기능 추가
    
    Binary Search Tree에 자식 노드가 몇 개인지에 따라 삭제를 다르게 하는 기능을 추가

---
+++ Chap11_Search1/Chap11_Search1/BinarySearchTree2.h
@@ -0,0 +1,27 @@
+#ifndef __BINARY_SEARCH_TREE2_H__
+#define __BINARY_SEARCH_TREE2_H__
+
+#include "BinaryTree3.h"
+
+typedef BTData BSTData;
+
+// BinarySearchTree의 생성 및 초기화
+void BSTMakeAndInit(BTreeNode ** pRoot);
+
+// 노드에 저장된 데이터 반환
+BSTData BSTGetNodeData(BTreeNode * bst);
+
+// BST에 데이터 저장
+void BSTInsert(BTreeNode ** pRoot, BSTData data);
+
+// BST에서 데이터 탐색
+BTreeNode * BSTSearch(BTreeNode * bst, BSTData target);
+
+// 트리에서 노드를 제거하고 제거된 노드의 주소 값을 반환
+BTreeNode * BSTRemove(BTreeNode ** pRoot, BSTData target);
+
+// BST에 저장된 모든 노드의 데이터 출력
+void BSTShowAll(BTreeNode * bst);
+
+#endif // !__BINARY_SEARCH_TREE2_H__
+
